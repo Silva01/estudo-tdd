358ce59807cc726a535e6f971706804cd8f868a8
silva01 <silva20102@gmail.com>
2022-01-18 19:32:34 -0300

    Initial Commit

---
+++ src/main/java/br/net/daniel/silva/estudo/tdd/estudotdd/EstudoTddApplication.java
@@ -0,0 +1,13 @@
+package br.net.daniel.silva.estudo.tdd.estudotdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstudoTddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/net/daniel/silva/estudo/tdd/estudotdd/EstudoTddApplicationTests.java
@@ -0,0 +1,13 @@
+package br.net.daniel.silva.estudo.tdd.estudotdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
