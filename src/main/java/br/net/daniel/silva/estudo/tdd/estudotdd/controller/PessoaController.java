da21b6b4bd76d8f2e9c79b0c3828e23ae728d9c3
daniel.cunha <daniel.cunha@redspark.io>
2022-01-21 16:01:19 -0300

    Adicionado Swagger

--- src/main/java/br/net/daniel/silva/estudo/tdd/estudotdd/controller/PessoaController.java
+++ src/main/java/br/net/daniel/silva/estudo/tdd/estudotdd/controller/PessoaController.java
@@ -2,12 +2,15 @@ package br.net.daniel.silva.estudo.tdd.estudotdd.controller;
 
 import br.net.daniel.silva.estudo.tdd.estudotdd.models.Pessoa;
 import br.net.daniel.silva.estudo.tdd.estudotdd.services.PessoaService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(value = "API para estudo do TDD")
 @RestController
 @RequestMapping("/pessoa")
 public class PessoaController {
@@ -19,16 +22,19 @@ public class PessoaController {
         this.service = service;
     }
 
+    @ApiOperation(value = "API para criar Pessoa", consumes = "application/json")
     @PostMapping
     public Pessoa create(@RequestBody Pessoa pessoa) {
         return service.create(pessoa);
     }
 
+    @ApiOperation(value = "API para Listar Pessoas", consumes = "application/json")
     @GetMapping
     public List<Pessoa> findAll() {
         return service.findAll();
     }
 
+    @ApiOperation(value = "API para excluir Pessoa")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void remove(@PathVariable("id") Long id) {
